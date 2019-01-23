e7105eca34c5ac81dcb10889adeccc690f7496b9
AlejandroMarin <david.marin@sofka.com.co>
2019-01-23 16:35:18 -0500

    add route service funtionality

--- src/services/route.js
+++ src/services/route.js
@@ -1,3 +1,12 @@
+const ID_CONTAINER = 'container';
+const ID_MENU_CONTAINER = 'menu';
+
 class Route{
-    
+    static routeTo(path){
+        render(path);
+    }
+}
+
+function getElementAndRender(path, idElement=ID_CONTAINER){
+    $(`#${idElement}`).load("./src/pages/menu/template.html");
 }
