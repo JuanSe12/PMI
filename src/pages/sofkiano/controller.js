e4dab192059fa733e756f5f0e3b6e70fff447ee9
juan alberto zapata zapata <juan.zapata@sofka.com.co>
2019-01-24 09:58:37 -0500

    new visual features

--- src/pages/sofkiano/controller.js
+++ src/pages/sofkiano/controller.js
@@ -13,40 +13,24 @@ class Data {
 
 
     fill() {
-        var div = document.getElementById("sofkianos-list");
+        var ul = document.getElementById("sofkianos-list");
         let template = "";
 
         /*sofkianos.forEach(sofkiano => {
             sofkiano.lastName
-        });*/
+        });*/ 
 
         for (var i = 0; i < 10; i++) {
             let li = 
             `<li class="collection-item avatar">
-                <div class="collapsible-header"><img src="../../public/images/yuna.JPG" alt="" class="circle">First</div>
-                <div class="collapsible-body">${sofkiano.firtsName } ${sofkiano.lastName}</div>
+                <div class="collapsible-header flow-text"><img src="../../assets/images/person.png" alt="" class="resize circle">First</div>
+                <div class="collapsible-body flow-text">${sofkiano.firtsName } ${sofkiano.lastName}</div>
             </li>`;
            template += li;     
         }
-        div.innerHTML = template;
+        ul.innerHTML = template;
         
     }
-
-    creatingElements() {
-        let li = document.createElement("LI");
-        let div1 = document.createElement("DIV");
-        let div2 = document.createElement("DIV");
-        let img = document.createElement("IMG");
-        let span = document.createElement("SPAN");
-        let divInfo = document.createElement("div");
-        let pName = document.createElement("p");
-        let pDocType = document.createElement("p");
-        let pDocNumber = document.createElement("p");
-        let pFeature = document.createElement("p");
-        let pTecno = document.createElement("p");
-        return [li, div1, div2, img, span, divInfo, pName, pDocType, pDocNumber, pFeature, pTecno];
-    }
-
 
 }
 
