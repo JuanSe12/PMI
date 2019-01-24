a4ff1fc333bb0e7394c382cb35ce25518e8d2323
Melissa barba <melibarba4@gmail.com>
2019-01-24 13:41:07 -0500

    Sofkiano card updates
    Muestra las habilidades y tecnologias

--- src/pages/sofkiano/controller.js
+++ src/pages/sofkiano/controller.js
@@ -2,8 +2,12 @@
 
 let sofkiano = {
     id: 1,
-    firtsName: "pepito",
-    lastName: "perez"
+    firtsName: "Pepito",
+    lastName: "Perez",
+    docType: "Cédula",
+    docNumber: "100203",
+    skills: ["Genial","Mucho más genial","Amable","Liderazgo","Buena actitud","blabla"],
+    tecnos: [{name:"Angular", img:"../../assets/images/tecnologhies/angular.png"},{name:"CSS", img:"../../assets/images//tecnologhies/css.png"}]
 }
 
 class Data {
@@ -20,16 +24,79 @@ class Data {
             sofkiano.lastName
         });*/ 
 
-        for (var i = 0; i < 10; i++) {
+        for (let indexSofkiano = 0; indexSofkiano < 10; indexSofkiano++) {
+
+
+            let tecnologhies= this.fillTecno(indexSofkiano);
+            let skills = this.fillSkills(indexSofkiano);
+
             let li = 
             `<li class="collection-item avatar">
-                <div class="collapsible-header flow-text"><img src="../../assets/images/person.png" alt="" class="resize circle">First</div>
-                <div class="collapsible-body flow-text">${sofkiano.firtsName } ${sofkiano.lastName}</div>
+                <div class="collapsible-header"><img src="../../assets/images/person.png" 
+                alt="" class="resize circle"> ${sofkiano.firtsName } ${sofkiano.lastName}</div>
+               
+                <div class="collapsible-body ">               
+                <form class="col s12">
+                  <div class="row">
+                    <div class="input-field col s6">
+                      <input disabled placeholder=${sofkiano.docType } id="first_name" type="text" class="validate">
+                      <label for="first_name">Document type</label>
+                    </div>
+                    <div class="input-field col s6">
+                    <input disabled placeholder=" ${sofkiano.docNumber }" id="" type="text" class="validate">
+                    <label for="first_name">Document number</label>
+                    </div>      
+                  </div>           
+                  <p>Personal characteristics</p>
+                  `+ skills +`
+                  <p>Technologies</p>
+                    `+ tecnologhies +`
+                  <div class="row">
+                    <div class="input-field col s6">
+                      <input disabled placeholder=${sofkiano.docType } id="" type="text" class="validate">
+                      <label for="first_name">Time experience in Sofka</label>
+                    </div>
+                    <div class="input-field col s6">
+                    <input disabled placeholder=" ${sofkiano.docNumber }" id="" type="text" class="validate">
+                    <label for="first_name">External time experience </label>
+                    </div>      
+                  </div>  
+                </form>                
+                </div>
             </li>`;
            template += li;     
         }
         ul.innerHTML = template;
         
+    }
+
+    fillTecno(indexSofkiano){
+        let tecnoTemplate= "";
+        for (let j = 0; j < sofkiano.tecnos.length; j++) {
+            let tecnoChips =  
+                `<div class="chips">                  
+                    <div class="chip">                  
+                        <img src="${sofkiano.tecnos[j].img }" alt=""> 
+                        ${sofkiano.tecnos[j].name }
+                    </div>
+                </div> `;
+                tecnoTemplate += tecnoChips;
+        }
+        return tecnoTemplate;
+    }
+
+    fillSkills(indexSofkiano){
+        let skillTemplate= "";
+        for (let j = 0; j < sofkiano.skills.length; j++) {
+            let skillChips =  
+                `<div class="chips">                  
+                    <div class="chip">                                         
+                        ${sofkiano.skills[j] }
+                    </div>
+                </div> `;
+                skillTemplate += skillChips;
+        }
+        return skillTemplate;
     }
 
 }
