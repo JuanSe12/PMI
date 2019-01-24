c71801f956b6d842afdc30ad18ef5024821574e6
AlejandroMarin <david.marin@sofka.com.co>
2019-01-24 08:43:05 -0500

    fix class project: add  technology property

--- src/model/project.js
+++ src/model/project.js
@@ -1,10 +1,11 @@
 class Project{
-    constructor(id, name, description, state, client){
+    constructor(id, name, description, state, client, thechnologies){
         this.id = id;
         this.name = name;
         this.description = description;
         this.state = state;
         this.client = client;
+        this.thechnologies = thechnologies
     }
 
     getState(){
