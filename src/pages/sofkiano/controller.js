37c359787ca5df5b41ce08a4392a845f021221cd
Melissa barba <melibarba4@gmail.com>
2019-01-24 08:14:02 -0500

    cambios en las tarjetas sofkianos

--- src/pages/sofkiano/controller.js
+++ src/pages/sofkiano/controller.js
@@ -1,53 +1,69 @@
 'use strict';
-class Data{
+class Data {
 
-    constructor(){
+    constructor() {
 
     }
 
-    fill(){
-        
-        let ul=document.getElementById("myList");    
-        for(var i=0;i<10;i++){
-            
-            let [li,div1,div2,img,pan]=this.creatingElements();        
-    
-            li.setAttribute("class","collection-item avatar");
-       
-            img.setAttribute("class","circle");
-            img.setAttribute("src", "../../public/images/yuna.JPG");
-            img.setAttribute("alt","Not avaible")
-    
-            div1.setAttribute("class","collapsible-header");
-            div2.setAttribute("class","collapsible-body");
-           
-       
+    fill() {
+
+        let ul = document.getElementById("myList");
+        for (var i = 0; i < 10; i++) {
+
+            let [li, div1, div2, img, span, divInfo, pName, pDocType, pDocNumber, pFeature, pTecno] = 
+                this.creatingElements();
+
+            li.setAttribute("class", "collection-item avatar");
+
+            img.setAttribute("class", "circle ");
+            img.setAttribute("src", "../../assets/images/person.png ");
+            img.setAttribute("alt", "Not avaible");
+
+
+            div1.setAttribute("class", "collapsible-header");
+            div2.setAttribute("class", "collapsible-body");
+            divInfo.setAttribute("class", "sofkiano-info");
+            span.setAttribute("class","tittle");
+            span.appendChild(document.createTextNode("Names"));
+
             div1.appendChild(img);
-            div1.appendChild(document.createTextNode("Hola"));
-          
-            pan.appendChild(document.createTextNode("Lorem ipsum dolor sit amet."));
-            div2.appendChild(pan);
+            div1.appendChild(span);
+           // span.appendChild(document.createTextNode("Lorem ipsum dolor sit amet."));
+            // div2.appendChild(span);
+            div2.appendChild(divInfo);
+
+            
            
+            divInfo.appendChild(pDocType);
+            pDocType.appendChild(document.createTextNode("Document type"));
+            divInfo.appendChild(pDocNumber);
+            pDocNumber.appendChild(document.createTextNode("Document number"));
+
             li.appendChild(div1);
             li.appendChild(div2);
-       
             ul.appendChild(li);
-    
-        }        
+
+        }
     }
 
-    creatingElements(){
-        let li=document.createElement("LI");
-        let div1=document.createElement("DIV");
-        let div2=document.createElement("DIV");
-        let img=document.createElement("IMG");
-        let pan=document.createElement("SPAN");
-        return [li,div1,div2,img,pan];
+    creatingElements() {
+        let li = document.createElement("LI");
+        let div1 = document.createElement("DIV");
+        let div2 = document.createElement("DIV");
+        let img = document.createElement("IMG");
+        let span = document.createElement("SPAN");
+        let divInfo = document.createElement("div");
+        let pName = document.createElement("p");
+        let pDocType = document.createElement("p");
+        let pDocNumber = document.createElement("p");
+        let pFeature = document.createElement("p");
+        let pTecno = document.createElement("p");
+        return [li, div1, div2, img, span, divInfo, pName, pDocType, pDocNumber, pFeature, pTecno];
     }
 
 
 }
 
-var data=new Data();
+var data = new Data();
 
 data.fill();
