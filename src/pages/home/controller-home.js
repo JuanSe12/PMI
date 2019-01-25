9d77efcf199f3109f89ad2de14160ad2e4593a56
Giovanny <gio96gomez@gmail.com>
2019-01-25 14:08:35 -0500

    Gio updates

--- src/pages/home/controller-home.js
+++ src/pages/home/controller-home.js
@@ -1,17 +1,10 @@
 'use strict';
 
-let sofkiano = {
-    id: 1,
-    firtsName: "Pepito",
-    lastName: "Perez",
-    docType: "Cédula",
-    docNumber: "100203",
-    tecnos: [{name:"Angular", img:"../../assets/images/tecnologhies/angular.png"},{name:"CSS", img:"../../assets/images//tecnologhies/css.png"}]
-}
-
 let business = {
     id:1,
-    client: [{name:"Alpina", img:"./angular.png", projectName: "alpinita"}]
+    client: [{name:"Angular", img:"./angular.png", projectName: "Angular Project 1",estado:"Completo"},
+    {name:"SAP", img:"./SAP.png", projectName: "SAP Project 1",estado: "Incompleto"},
+    {name:"Default", img:"./default-image.png", projectName: "Default Project 1",estado: "Incompleto"}]   
 
 }
 
@@ -25,29 +18,25 @@ class Data {
         var ul = document.getElementById("business-list");
         let template = "";
 
-        for (let index = 0; index < 10; index++) {
-
-
-            let businessdata= this.fillBusiness(index);            
-
+        for (let index = 0; index < business.client.length; index++) {
             let li = 
             `<li class="collection-item avatar">
             <div class="collapsible-header">
                 <div class="col s12">
                     <div class="row">
                         <div class="col s2">
-                            <img src="./angular.png" alt="" class="resize circle">
+                            <img src="${business.client[index].img}" alt="" class="resize circle">
                         </div>
                         <div class="col s8">
                             <div>
-                                <h4>${business.client[0].projectName}</h4>
+                                <h4>${business.client[index].projectName} </h4>
                             </div>
                             <div>
-                                ${business.client[0].name }
+                                ${business.client[index].name}
                             </div>
                         </div>
                         <div class="col s2 vertical">
-                            <i>Estado</i>                           
+                            <i>${business.client[index].estado}</i>                           
                         </div>
                     </div>
                 </div>
@@ -56,22 +45,6 @@ class Data {
            template += li;     
         }
         ul.innerHTML = template;
-        
-    }
-
-    fillBusiness(index){
-        let businessTemplate= "";
-        for (let j = 0; j < business.client.length; j++) {
-            /*let data =  
-                `<div class="chips">                  
-                    <div class="chip">
-                        ${business.client[0].name }
-                    </div>
-                </div> `;
-                businessTemplate += data;*/
-                console.log(business.client[0].name);
-        }
-        return businessTemplate;
     }
 }
 
