8421e850acdf4db0b5251188a33dbcd472a33c2f
AlejandroMarin <david.marin@sofka.com.co>
2019-01-29 13:22:19 -0500

    filter finish

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import Route from "./services/route.js";
-import DataService from "./services/data_service.js"
+
 
 Route.renderMenu();
 Route.routeTo('project');
 
-document.getElementById('input-filter').onchange = function(event){
-    console.log(this.value)
 
-    DataService.getAllSofkianos().then(
-        sofkianos => {
-            let datos = new Set()
-            sofkianos.forEach(sofkiano => {
-                
-            });
-        }
-    )
-
-}
