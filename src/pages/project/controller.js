ef9e8b21a7cecad8bac08e383ef067ee69bfec8d
juan alberto zapata zapata <juan.zapata@sofka.com.co>
2019-01-29 13:34:13 -0500

    view-projects

--- src/pages/project/controller.js
+++ src/pages/project/controller.js
@@ -46,24 +46,4 @@ export default async function fillProjects() {
                 ul.innerHTML = template;},150);         
             
         })
-        
 }
-
-function fillTecno(projects) {
-    let tecnoTemplate = "";
-    for (let project of projects) {
-        let tecnoChips =
-            `<div class="chips">                  
-            <div class="chip">                  
-                <img src="${project.icon}" alt=""> 
-                ${project.name}
-            </div>
-        </div> `;
-        tecnoTemplate += tecnoChips;
-        //console.log(project.name);
-    }
-    return tecnoTemplate;
-}
-
-
-
