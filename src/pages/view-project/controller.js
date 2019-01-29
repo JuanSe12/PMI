d5ec353154c4e1093d4757140cd154aa6091c531
Giovanny <gio96gomez@gmail.com>
2019-01-29 15:41:20 -0500

    listSofkianos

--- src/pages/view-project/controller.js
+++ src/pages/view-project/controller.js
@@ -1,9 +1,10 @@
 import DataService from "../../services/data_service.js";
+import Config from "../../config/config.js"
 
 export default function fillProject() {
     DataService.getProjectByIds([1]).then(
         projects => {
-            var TechnologiesContent = document.getElementById("container-tech");            
+            var TechnologiesContent = document.getElementById("container-tech");
             let sofkianosContent = document.getElementById("project-sofkianos-list");
             let template = "";
             let project = projects[0];
@@ -14,27 +15,36 @@ export default function fillProject() {
             //console.log(project)
             let nameBusiness = "";
 
+
+            let prueba = document.getElementById('project-business-image');
+
             project.getClient().then(
                 client => {
                     //console.log(client[0].name);
-                    document.getElementById('business-name').innerText = client[0].name;
+                    let clientStr = `<img src="${Config.baseUrl() + client[0].img}" alt="" class="resize circle" id="project-business-image">
+                    ${client[0].name}`
+                    document.getElementById('business-name').innerHTML = clientStr;
                 }
             )
 
-            project.getSofkianos().then(
+           project.getSofkianos().then(
                 sofkianos => {
                     //console.log(sofkianos);
                     sofkiano = fillSofkianos(sofkianos);
                     //console.log(sofkiano);
-                    sofkianosContent.innerHTML = sofkiano;                    
-            })
+                    let addButton = `<li class="list-sofkianos">
+                                <a class="btn-floating center-image"><i class="material-icons">add</i></a>
+                            </li>`;
+                    sofkianosContent.innerHTML = sofkiano + addButton;
+                })
 
-            
+
 
             document.getElementById('title-project').innerText = project.name;
             document.getElementById('input-project-objetive').value = project.description;
             document.getElementById('input-project-start-date').value = startDate;
             document.getElementById('input-project-finish-date').value = finishDate;
+            
 
 
             project.getTechnologies().then(tech => {
@@ -43,7 +53,7 @@ export default function fillProject() {
             })
 
         })
-        
+
 }
 
 function fillTecno(projects) {
@@ -66,11 +76,12 @@ function fillSofkianos(sofkiano) {
     let sofkianoTemplate = "";
     for (let sofki of sofkiano) {
         let sofkianoList =
-            `<li class="collection-item avatar">
-            <img src="" alt="" class="resize circle ">
-            <br>
-            ${sofki.firtsName}
+            `<li class="collection-item avatar list-sofkianos">
+                <img src="${Config.baseUrl() + sofki.img}" alt="" class="circle ">
+                <br>
+                ${sofki.firtsName}
             </li>`;
+        //console.log(sofkianoList);
         sofkianoTemplate += sofkianoList;
     }
     return sofkianoTemplate;
