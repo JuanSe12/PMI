2d3b0ce814f7053bc5dadab8dd39c83d5734e220
Giovanny <gio96gomez@gmail.com>
2019-01-29 13:15:02 -0500

    ImagesProjects

--- src/pages/project/controller.js
+++ src/pages/project/controller.js
@@ -4,74 +4,47 @@ import DataService from "../../services/data_service.js";
 export default function fillProjects() {
     DataService.getProjectByIds([1]).then(
         projects => {
-            var content = document.getElementById("container-tech");
+            var TechnologiesContent = document.getElementById("container-tech");            
+            let sofkianosContent = document.getElementById("project-sofkianos-list");
             let template = "";
             let project = projects[0];
             let startDate = project.getDateInit();
             let finishDate = project.getDateFinish();
-            let tecnologhies = "";
-            console.log(project)
-            let nameBusiness= "";
+            let technologies = "";
+            let sofkiano = "";
+            //console.log(project)
+            let nameBusiness = "";
+
             project.getClient().then(
-                client =>{
-                    console.log(client[0])
+                client => {
+                    //console.log(client[0].name);
+                    document.getElementById('business-name').innerText = client[0].name;
                 }
             )
+
+            project.getSofkianos().then(
+                sofkianos => {
+                    //console.log(sofkianos);
+                    sofkiano = fillSofkianos(sofkianos);
+                    //console.log(sofkiano);
+                    sofkianosContent.innerHTML = sofkiano;                    
+            })
+
             
+
             document.getElementById('title-project').innerText = project.name;
             document.getElementById('input-project-objetive').value = project.description;
             document.getElementById('input-project-start-date').value = startDate;
             document.getElementById('input-project-finish-date').value = finishDate;
-           // document.getElementById('business-name').value = project.;
 
 
-            project.getTechnologies().then(tech=>{
-                tecnologhies = fillTecno(tech);
-            }).then(tech=>{
-                template += tecnologhies;
-            }).then(tech =>{
-                content.innerHTML = template;
-            })   
-    })
-   /* DataService.getAllProjects().then(projects => {
-            console.log("Entró al promise");
-
-            var content = document.getElementById("project-view");
-            let template = "";
-
-            console.log(projects[0]);
-
-            projects.map(project => {
-                
-
-                //let div = 
-              /*  let li =
-                    `<div class="collection-item avatar">
-            <div class="collapsible-header">
-                <div class="col s12">
-                    <div class="row">
-                        <div class="col s2">
-                            <img src="${project.img}" alt="" class="resize circle">
-                        </div>
-                        <div class="col s8">
-                            <div>
-                                ${project.name} 
-                            </div>
-                            <div>
-                                ${project.client}
-                            </div>
-                        </div>
-                        <div class="col s2 vertical">
-                            ${project.state}                           
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>`;
-               // template += div;
+            project.getTechnologies().then(tech => {
+                technologies = fillTecno(tech);
+                TechnologiesContent.innerHTML = technologies;
             })
-           //content.innerHTML = template;
-        })*/
+
+        })
+        
 }
 
 function fillTecno(projects) {
@@ -85,7 +58,21 @@ function fillTecno(projects) {
             </div>
         </div> `;
         tecnoTemplate += tecnoChips;
-        console.log(project.name);
+        //console.log(project.name);
     }
     return tecnoTemplate;
+}
+
+function fillSofkianos(sofkiano) {
+    let sofkianoTemplate = "";
+    for (let sofki of sofkiano) {
+        let sofkianoList =
+            `<li class="collection-item avatar">
+            <img src="" alt="" class="resize circle ">
+            <br>
+            ${sofki.firtsName}
+            </li>`;
+        sofkianoTemplate += sofkianoList;
+    }
+    return sofkianoTemplate;
 }
