7874062dbfe0edc5333505882185745c498b967e
AlejandroMarin <david.marin@sofka.com.co>
2019-01-30 17:32:27 -0500

    add project percent in sofkiano

--- src/model/sofkiano.js
+++ src/model/sofkiano.js
@@ -1,13 +1,14 @@
 import DataService from "../services/data_service.js";
 
-export default class Sofkiano{
-    constructor(id, firtsName, lastName, img,projects, documentType, documentNumber, 
-        externalExperience, internalExperience, feactures, technologies){
+export default class Sofkiano {
+    constructor(id, firtsName, lastName, img, projects, percents, documentType, documentNumber,
+        externalExperience, internalExperience, feactures, technologies) {
         this.id = id;
         this.firtsName = firtsName;
         this.lastName = lastName;
         this.img = img;
-        this.projects=projects;
+        this.projects = projects;
+        this.percents = percents
         this.documentType = documentType;
         this.documentNumber = documentNumber;
         this.externalExperience = externalExperience;
@@ -17,16 +18,33 @@ export default class Sofkiano{
     }
 
 
-    getFeatures(){
-       return DataService.getFeaturesByIds(this.feactures);
+    getFeatures() {
+        return DataService.getFeaturesByIds(this.feactures);
     }
 
 
-    getTechnologies(){
+    getTechnologies() {
         return DataService.getTechnologiesByIds(this.technologies);
     }
 
-    getProjects(){
-        return DataService.getProjectSofkianoByIds(this.projects);
+    getProjects() {
+        return new Promise((resolve, reject) => {
+            DataService.getProjectByIds(this.projects)
+                .then(
+                    projects => {
+                        if(this.percents){
+                            projects.forEach((project, index) => {
+                                project['percent'] = (this.percents[index])
+                            });
+                        } 
+                        resolve(projects);
+                    }
+                )
+                .catch(
+                    error => {
+                        reject(error);
+                    }
+                );
+        });
     }
 }
