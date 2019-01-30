dcee11243e01891358c9970949275b51833999f3
Juanse <juan147123@gmail.com>
2019-01-30 10:42:26 -0500

    se agregó el controlador guardado de sofkiano, la resolución de conflictos con la rama (meli) y el archivo init.js modificado v2 branch develop

--- src/init.js
+++ src/init.js
@@ -1,10 +1,10 @@
 
 
 import fillSofkiano from "./pages/sofkiano/controller.js";
+import DOMsaveSofkiano from "./pages/sofkiano/create_sofkiano.js";
 import fillProjects from "./pages/project/controller.js";
 import fillClient from "./pages/client/clientController.js";
 
-//import fillClients from "./pages/client/clientController.js";
 let init = {}
 
 Object.setPrototypeOf(init, {
