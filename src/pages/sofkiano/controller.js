bfd735b70e20513cc7883aef0ded3c48a606c4ac
juan alberto zapata zapata <juan.zapata@sofka.com.co>
2019-02-01 14:14:43 -0500

    something especial

--- src/pages/sofkiano/controller.js
+++ src/pages/sofkiano/controller.js
@@ -171,3 +171,33 @@ function DomDeleteSofkiano(sofkianos) {
 
     });
 }
+
+const validateFieldsForMessage=()=>{
+    $("#last_name").blur(function () {
+        if ($("#last_name").val() === "") {
+           M.toast({ html: 'Ingrese los apellidos' });
+        }
+     });
+    
+     $("#number_document").blur(function () {
+        if ($("#number_document").val() === "") {
+           M.toast({ html: 'Ingrese el documento de identidad' });
+        }
+     });
+
+     $("#experience_time_sofka").blur(function () {
+        if ($("#experience_time_sofka").val() === "") {
+           M.toast({ html: 'Ingrese los meses de experiencia en Sofka' });
+        }
+     });
+
+     $("#external_experience_time").blur(function () {
+        if ($("#external_experience_time").val() === "") {
+           M.toast({ html: 'Ingrese los meses de experiencia en empresas externas' });
+        }
+     });
+
+}
+setTimeout(() => {
+    validateFieldsForMessage();
+}, 180);
